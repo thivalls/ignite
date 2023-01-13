383a7aa5d3f4a30c84219979f4a4b70aae3578b9
valls <olavalls@gmail.com>
2023-01-13 12:54:36 -0300

    Creating a middlaware to know how it works

--- aulas/http/src/server.js
+++ aulas/http/src/server.js
@@ -1,21 +1,12 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-    const buffers = []
-
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
-
-    try {    
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-    }
+    await json(req, res)
 
     if (url === "/create" && method === "POST") {
         users.push(req.body)
@@ -23,9 +14,7 @@ const server = http.createServer(async (req, res) => {
     }
 
     if (url === "/list" && method === "GET") {
-        return res
-            .setHeader('Content-type', 'application/json')
-            .end(JSON.stringify(users))
+        return res.end(JSON.stringify(users))
     }
 
     return res.writeHead(404).end('Route not founded')
